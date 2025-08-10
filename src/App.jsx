e77f0f8f92c7e975983142b13bc4c8693f887b38
Oumaima Jemai <oussamabenzattat@gmail.com>
2025-08-10 22:12:52 +0100

    new version

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherApp from './Myfiles/firstTry'
+
+function App() {
+ 
+  return (
+    <div className='container  w-full h-screen bg-[#e2d4ff] flex justify-center items-center'>
+      <WeatherApp/>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
